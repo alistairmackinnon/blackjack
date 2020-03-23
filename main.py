bf46bb0421e6b34f65b9b455c88d6c4be5606d0a
Ali <alistair.mackinnon@outlook.com>
2020-03-23 00:01:03 +0000

    working blackjack - ace rules missing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+from random import shuffle
 
 
 def generate_deck():
@@ -24,45 +24,78 @@ def generate_deck():
                 value = num
             key = f'{name} of {suit}'
             deck = deck + [(key, value)]
+    shuffle(deck)
     return deck
 
 
-def get_card(input_deck):
-    num_items = len(input_deck)
-    index = random.randint(0, num_items - 1)
-    card = input_deck[index]
-    del input_deck[index]
+def deal_card(input_deck):
+    card = input_deck[0]
+    del input_deck[0]
     return card
 
 
-def deal_cards(input_deck, cards_to_deal):
+def evaluate_score(input_lst):
     score = 0
-    for i in range(cards_to_deal):
-        card = get_card(input_deck)
-        score += card[1]
+    for card, value in input_lst:
+        score += value
     return score
 
 
-def main():
-    deck = generate_deck()
-    dealer_score = deal_cards(deck, 2)
-    player_score = deal_cards(deck, 2)
-    print(f'Dealer has {dealer_score}')
-    print(f'Player has {player_score}, stick (s) or twist (t)?')
-    while True:
-        response = input()
-        if response == 't':
-            player_score += deal_cards(deck, 1)
-            if player_score == 21:
-                print('Blackjack')
-                break
-            if player_score > 21:
-                print(f'Bust - you got {player_score}')
-                break
-            else:
-                print(f'Player has {player_score}, stick (s) or twist (t)?')
-        if response == 's':
-            break
+def blackjack_bust_check(input_score):
+    if input_score == 21:
+        result = 'Blackjack!'
+    elif input_score > 21:
+        result = 'Bust!'
+    else:
+        result = False
+    return result
 
+
+players = ['Dealer', 'Ali']
+
+
+def main():
+    dealer_hand = []
+    player_hand = []
+    deck = generate_deck()
+    for i in range(2):
+        for player in players:
+            card = deal_card(deck)
+            if player == 'Dealer':
+                dealer_hand.append(card)
+            else:
+                player_hand.append(card)
+    print(f'Dealer\'s first card is {dealer_hand[0][0]}')
+    print(f'Dealers current score is {dealer_hand[0][1]}')
+    player_score = evaluate_score(player_hand)
+    while True:
+        print(f'Your score is {player_score}')
+        print('Stick (s) or twist (t)?')
+        response = input()
+        if response == 's':
+            player_blackjack_bust = blackjack_bust_check(player_score)
+            break
+        elif response == 't':
+            card = deal_card(deck)
+            player_hand.append(card)
+            player_score = evaluate_score(player_hand)
+            player_blackjack_bust = blackjack_bust_check(player_score)
+            if player_blackjack_bust:
+                break
+
+    dealer_score = evaluate_score(dealer_hand)
+    while True:
+        if dealer_score < 17:
+            card = deal_card(deck)
+            dealer_hand.append(card)
+            dealer_score = evaluate_score(dealer_hand)
+        else:
+            break
+    dealer_blackjack_bust = blackjack_bust_check(dealer_score)
+
+    if player_blackjack_bust:
+        print(f'Player Result: {player_blackjack_bust}... Score {player_score}')
+    else:
+        print(f'Player Score = {player_score}')
 
 main()
